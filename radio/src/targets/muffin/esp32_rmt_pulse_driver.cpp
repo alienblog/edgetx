3a24170674852507be3066cb5d23864c90a1dd3a
alien <alienblog@outlook.com>
2023-08-08 09:50:08 +0800

    fix type

--- radio/src/targets/muffin/esp32_rmt_pulse_driver.cpp
+++ radio/src/targets/muffin/esp32_rmt_pulse_driver.cpp
@@ -37,26 +37,31 @@ static void esp32_rmt_tx_task(void * pdata) {
 }
 #endif
 
-static void esp32_rmt_rx_task(void * pdata) {
-    rmt_ctx_t *ctx = (rmt_ctx_t *)pdata;
-    rmt_symbol_word_t raw_symbols[512];
-    rmt_rx_done_event_data_t rx_data;
+static void esp32_rmt_rx_task(void *pdata)
+{
+  rmt_ctx_t *ctx = (rmt_ctx_t *)pdata;
+  rmt_symbol_word_t raw_symbols[512];
+  rmt_rx_done_event_data_t rx_data;
 
-    rmt_enable(ctx->rmt);
-    while(!ctx->exit) {
-        // ready to receive
-        ESP_ERROR_CHECK(rmt_receive(ctx->rmt, raw_symbols, sizeof(raw_symbols), &ctx->rx_cfg));
-        xQueueReceive(ctx->rxQueue, &rx_data, portMAX_DELAY);
-        if (!ctx->exit) {
-            ctx->decoder(ctx, &rx_data);
-        }
-     }
-    esp32_rmt_ctx_free(ctx); // done with the ctx
-    vTaskDelete(NULL);
+  rmt_enable(ctx->rmt);
+  while (!ctx->exit) {
+    // ready to receive
+    ESP_ERROR_CHECK(
+        rmt_receive(ctx->rmt, raw_symbols, sizeof(raw_symbols), &ctx->rx_cfg));
+    xQueueReceive(ctx->rxQueue, &rx_data, portMAX_DELAY);
+    if (!ctx->exit) {
+      ctx->decoder(ctx, &rx_data);
+    }
+  }
+  esp32_rmt_ctx_free(ctx);  // done with the ctx
+  vTaskDelete(NULL);
 }
 
-rmt_ctx_t *esp32_rmt_tx_init(rmt_ctx_t *ctxmem, int pin, rmt_reserve_memsize_t memsize, float tick_in_ns, rmt_tx_encode_cb_t enc_fn, size_t pulse_in_frame) {
-    rmt_ctx_t *rvalue = ctxmem;
+rmt_ctx_t *esp32_rmt_tx_init(rmt_ctx_t *ctxmem, int pin,
+                             rmt_reserve_memsize_t memsize, float tick_in_ns,
+                             rmt_tx_encode_cb_t enc_fn, size_t pulse_in_frame)
+{
+  rmt_ctx_t *rvalue = ctxmem;
 #if 0
     if (NULL != rvalue) {
         rvalue->encoder = enc_fn;
@@ -73,84 +78,103 @@ rmt_ctx_t *esp32_rmt_tx_init(rmt_ctx_t *ctxmem, int pin, rmt_reserve_memsize_t m
         }
     }
 #endif
-    return rvalue;
+  return rvalue;
 }
 
-static bool rmt_rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *user_data)
+static bool rmt_rx_done_callback(rmt_channel_handle_t channel,
+                                 const rmt_rx_done_event_data_t *edata,
+                                 void *user_data)
 {
-    BaseType_t high_task_wakeup = pdFALSE;
-    QueueHandle_t receive_queue = (QueueHandle_t)user_data;
-    // send the received RMT symbols to the parser task
-    xQueueSendFromISR(receive_queue, edata, &high_task_wakeup);
-    return high_task_wakeup == pdTRUE;
+  BaseType_t high_task_wakeup = pdFALSE;
+  QueueHandle_t receive_queue = (QueueHandle_t)user_data;
+  // send the received RMT symbols to the parser task
+  xQueueSendFromISR(receive_queue, edata, &high_task_wakeup);
+  return high_task_wakeup == pdTRUE;
 }
 
-rmt_ctx_t *esp32_rmt_rx_init(rmt_ctx_t *ctxmem, int pin, rmt_reserve_memsize_t memsize, float tick_in_ns, rmt_rx_decode_cb_t dec_fn, size_t pulse_in_frame, size_t idle_threshold_in_ns, size_t min_pulse_in_ns) {
-    rmt_ctx_t *rvalue = ctxmem;
-
-    if (NULL != rvalue) {
-        rmt_rx_channel_config_t rx_channel_cfg = {
-            .gpio_num = pin,
-            .clk_src = RMT_CLK_SRC_DEFAULT,
-            .resolution_hz = (uint32_t)(1000000000 / tick_in_ns),
-            .mem_block_symbols = memsize, // amount of RMT symbols that the channel can store at a time
-        };
-        ESP_ERROR_CHECK(rmt_new_rx_channel(&rx_channel_cfg, &rvalue->rmt));
-
-        rvalue->decoder = dec_fn;
-        rvalue->rxQueue = xQueueCreate(1, sizeof(rmt_rx_done_event_data_t));
-        rvalue->exit = false;
-        if ((NULL != rvalue->rmt) && (NULL != rvalue->rxQueue)) {
-            rvalue->rx_cfg.signal_range_min_ns = min_pulse_in_ns;
-            rvalue->rx_cfg.signal_range_max_ns = idle_threshold_in_ns;
-            xTaskCreateStaticPinnedToCore(esp32_rmt_rx_task, "esp32_rmt_rx_task", sizeof(rvalue->rmt_task_stack)/sizeof(rvalue->rmt_task_stack[0]),
-                    rvalue, 5, rvalue->rmt_task_stack, rvalue->task_struct, TRAINER_PPM_OUT_TASK_CORE); // TODO-ESP32 priority
-            rmt_rx_event_callbacks_t cbs = {
-                .on_recv_done = rmt_rx_done_callback,
-            };
-            ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(rvalue->rmt, &cbs, rvalue->rxQueue));
-        } else {
-            free(rvalue);
-            rvalue = NULL;
-        }
-    }
-    return rvalue;
-}
-
-void esp32_rmt_stop(rmt_ctx_t *ctx) {
-    ctx->exit = true;
-    if (NULL != ctx->rxQueue) {
-        rmt_rx_done_event_data_t end = {0};
-        xQueueSend(ctx->rxQueue, &end, 0); // doesn't matter what was sent, just send to end the loop
-    }
-    // TODO-v5: no thing special need to be done for TX. It will end after the last frame sends out
-}
-
-static void esp32_rmt_ctx_free(rmt_ctx_t *ctx) {
-    if (NULL != ctx) {
-        rmt_del_channel(ctx->rmt);
-        free(ctx);
-    }
-}
-
-int rmt_ppm_decode_cb(rmt_ctx_t *ctx, rmt_symbol_word_t *rxdata, size_t rxdata_len, int16_t *ppm_decode_buf)
+rmt_ctx_t *esp32_rmt_rx_init(rmt_ctx_t *ctxmem, int pin,
+                             rmt_reserve_memsize_t memsize, float tick_in_ns,
+                             rmt_rx_decode_cb_t dec_fn, size_t pulse_in_frame,
+                             size_t idle_threshold_in_ns,
+                             size_t min_pulse_in_ns)
 {
-    int channel = 0;
-    uint32_t high = 0U;
-    for (size_t i = 0; i < rxdata_len; i++) {
-        if (i != 0) {
-            uint32_t val = (high + rxdata[i].duration0) * (ctx->tick_in_ns / 1000); // result in us
-            if (val > 800 && val < 2200) {
-                ppm_decode_buf[channel++] =
-                        (int16_t)(val - 1500) * (g_eeGeneral.PPM_Multiplier+10) / 10;  // +-500 != 512, but close enough.
-            } else {
-                channel = -1;
-                break;
-            }
-        }
-        high = rxdata[i].duration1;
+  rmt_ctx_t *rvalue = ctxmem;
+
+  if (NULL != rvalue) {
+    rmt_rx_channel_config_t rx_channel_cfg = {
+        .gpio_num = (gpio_num_t)pin,
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .resolution_hz = (uint32_t)(1000000000 / tick_in_ns),
+        .mem_block_symbols = memsize,  // amount of RMT symbols that the channel
+                                       // can store at a time
+    };
+    ESP_ERROR_CHECK(rmt_new_rx_channel(&rx_channel_cfg, &rvalue->rmt));
+
+    rvalue->decoder = dec_fn;
+    rvalue->rxQueue = xQueueCreate(1, sizeof(rmt_rx_done_event_data_t));
+    rvalue->exit = false;
+    if ((NULL != rvalue->rmt) && (NULL != rvalue->rxQueue)) {
+      rvalue->rx_cfg.signal_range_min_ns = min_pulse_in_ns;
+      rvalue->rx_cfg.signal_range_max_ns = idle_threshold_in_ns;
+      xTaskCreateStaticPinnedToCore(
+          esp32_rmt_rx_task, "esp32_rmt_rx_task",
+          sizeof(rvalue->rmt_task_stack) / sizeof(rvalue->rmt_task_stack[0]),
+          rvalue, 5, rvalue->rmt_task_stack, rvalue->task_struct,
+          TRAINER_PPM_OUT_TASK_CORE);  // TODO-ESP32 priority
+      rmt_rx_event_callbacks_t cbs = {
+          .on_recv_done = rmt_rx_done_callback,
+      };
+      ESP_ERROR_CHECK(
+          rmt_rx_register_event_callbacks(rvalue->rmt, &cbs, rvalue->rxQueue));
+    } else {
+      free(rvalue);
+      rvalue = NULL;
     }
-    return channel;
+  }
+  return rvalue;
+}
+
+void esp32_rmt_stop(rmt_ctx_t *ctx)
+{
+  ctx->exit = true;
+  if (NULL != ctx->rxQueue) {
+    rmt_rx_done_event_data_t end = {0};
+    xQueueSend(ctx->rxQueue, &end,
+               0);  // doesn't matter what was sent, just send to end the loop
+  }
+  // TODO-v5: no thing special need to be done for TX. It will end after the
+  // last frame sends out
+}
+
+static void esp32_rmt_ctx_free(rmt_ctx_t *ctx)
+{
+  if (NULL != ctx) {
+    rmt_del_channel(ctx->rmt);
+    free(ctx);
+  }
+}
+
+int rmt_ppm_decode_cb(rmt_ctx_t *ctx, rmt_symbol_word_t *rxdata,
+                      size_t rxdata_len, int16_t *ppm_decode_buf)
+{
+  int channel = 0;
+  uint32_t high = 0U;
+  for (size_t i = 0; i < rxdata_len; i++) {
+    if (i != 0) {
+      uint32_t val = (high + rxdata[i].duration0) *
+                     (ctx->tick_in_ns / 1000);  // result in us
+      if (val > 800 && val < 2200) {
+        ppm_decode_buf[channel++] = (int16_t)(val - 1500) *
+                                    (g_eeGeneral.PPM_Multiplier + 10) /
+                                    10;  // +-500 != 512, but close enough.
+      } else {
+        channel = -1;
+        break;
+      }
+    }
+    high = rxdata[i].duration1;
+  }
+  return channel;
 }
 
 void rmt_ppm_encode_cb(rmt_ctx_t *ctx, uint16_t *ppm, size_t len)
